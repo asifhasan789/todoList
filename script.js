11ec01dcb1edc7bc035b9a8bb218e8a4d1232222
asifhasan <asifaraz@gmail.com>
2023-12-10 02:43:36 +0530

    c3

--- script.js
+++ script.js
@@ -6,46 +6,69 @@ let taskBody = document.querySelector(".todoTasks");
 let titleVal = "";
 let descVal = "";
 
-window.onload = ()=>{
-      let dataGot = JSON.parse(localStorage.getItem("data")) || [];
-      displayTasks(dataGot)
+window.onload = () => {
+  let dataGot = JSON.parse(localStorage.getItem("data")) || [];
+  if(dataGot.length > 1){
+  displayTasks(dataGot);}
+};
 
-}
-
-titleInp.addEventListener("change", (e) => {
+titleInp.addEventListener("keyup", (e) => {
   titleVal = e.target.value;
+  if (e.key == "Enter") {
+    // console.log('enter got');
+    descInp.focus();
+  }
   // console.log(titleVal);
 });
-descInp.addEventListener("change", (e) => {
+descInp.addEventListener("keyup", (e) => {
   descVal = e.target.value;
+  if (e.key == "Enter") {
+    // console.log('enter got');
+    addTaskFunction();
+  }
   //   console.log(descVal);
 });
 
 let tasks = [];
 console.log(tasks);
 addTaskBtn.addEventListener("click", (e) => {
-  // localStorage.setItem("data", JSON.stringify(tasks));
-
 
   // let dataGot = JSON.parse(localStorage.getItem("data"));
+  //   let dataGot = JSON.parse(localStorage.getItem("data"));
+  //   console.log(dataGot) || []
+  //   tasks = dataGot
+
+  //   tasks.push({ title: titleVal, desc: descVal, done: false });
+  //   console.log(tasks);
+
+  //   localStorage.setItem("data", JSON.stringify(tasks));
+
+  addTaskFunction();
+  // displayTasks(dataGot)
+});
+
+function addTaskFunction() {
+  localStorage.setItem("data", JSON.stringify(tasks));
   let dataGot = JSON.parse(localStorage.getItem("data"));
-  console.log(dataGot) || []
-  tasks = dataGot
+  console.log(dataGot) || [];
+  tasks = dataGot;
 
   tasks.push({ title: titleVal, desc: descVal, done: false });
   console.log(tasks);
 
   localStorage.setItem("data", JSON.stringify(tasks));
 
+  displayTasks(dataGot);
+  titleInp.value = "";
+  descInp.value = "";
+}
 
-displayTasks(dataGot)
-
-});
-
-function displayTasks(data){
-    let displayItems = data.map((d,i) => {
-      let item = `<div class="taskI">
-<input type="checkbox" name="" id="${i}" onchange='toggleStrike(this,${i})' ${d.done?'checked':''}>
+function displayTasks(data) {
+  let displayItems = data.map((d, i) => {
+    let item = `<div class="taskI">
+<input type="checkbox" name="" id="${i}" onchange='toggleStrike(this,${i})' ${
+      d.done ? "checked" : ""
+    }>
 <div class="task">
     <p class="titleTxt">${d.title}</p>
     <p class="descTxt">${d.desc}</p>
@@ -53,54 +76,48 @@ function displayTasks(data){
 </div>
 <button class="delTask" onclick=delF(${i})>Delete</button>
 </div>`;
-      return item;
-    });
+    return item;
+  });
 
-    taskBody.innerHTML = displayItems.join("");
-
+  taskBody.innerHTML = displayItems.join("");
 }
 
-function toggleStrike(cb,i){
-
+function toggleStrike(cb, i) {
   let sb = cb.nextElementSibling;
   let dataGot = JSON.parse(localStorage.getItem("data"));
   console.log(dataGot);
   // if(i == dataGot.)
-  if(i in dataGot){
-  dataGot[i].done = !dataGot[i].done
+  if (i in dataGot) {
+    dataGot[i].done = !dataGot[i].done;
   }
   // if()
-  if(cb.checked){
-    sb.style.textDecoration = 'line-through'
-  }
-  else{
-    sb.style.textDecoration = 'none'
+  if (cb.checked) {
+    sb.style.textDecoration = "line-through";
+  } else {
+    sb.style.textDecoration = "none";
   }
   // console.log(cb,i);
   console.log(dataGot);
   localStorage.setItem("data", JSON.stringify(dataGot));
-
 }
 
-function delF(i){
-    let dataGot = JSON.parse(localStorage.getItem("data"));
-    console.log(dataGot);
-
-    dataGot.splice(i,1)
-
-    localStorage.setItem("data", JSON.stringify(dataGot));
-    displayTasks(dataGot)
-
-}
-
-function delCompTasks(){
+function delF(i) {
   let dataGot = JSON.parse(localStorage.getItem("data"));
   console.log(dataGot);
 
-  let undoneTasks = dataGot.filter((t)=>t.done == false)
+  dataGot.splice(i, 1);
+
+  localStorage.setItem("data", JSON.stringify(dataGot));
+  displayTasks(dataGot);
+}
+
+function delCompTasks() {
+  let dataGot = JSON.parse(localStorage.getItem("data"));
+  console.log(dataGot);
+
+  let undoneTasks = dataGot.filter((t) => t.done == false);
   console.log(undoneTasks);
-  displayTasks(undoneTasks)
+  displayTasks(undoneTasks);
 
-    localStorage.setItem("data", JSON.stringify(undoneTasks));
-
+  localStorage.setItem("data", JSON.stringify(undoneTasks));
 }
